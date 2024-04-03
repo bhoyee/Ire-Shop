82034d3b812dc52131ee9a8bfdaba7300b41acd0
unknown <s.adeboye@outlook.com>
2024-04-03 13:00:59 +0100

    updated product entity

--- server-ire-shop/src/product/entities/product.entity.ts
+++ server-ire-shop/src/product/entities/product.entity.ts
@@ -1,1 +1,12 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('product')
+export class ProductEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text')
+    title: string
+
+}
