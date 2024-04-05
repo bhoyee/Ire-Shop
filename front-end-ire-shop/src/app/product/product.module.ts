d234e365556264945198be62f118851323578ff9
unknown <s.adeboye@outlook.com>
2024-04-05 11:32:16 +0100

    front end initial commit

--- front-end-ire-shop/src/app/product/product.module.ts
+++ front-end-ire-shop/src/app/product/product.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRoutingModule } from './product-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductRoutingModule
   ]
 })
 export class ProductModule { }
