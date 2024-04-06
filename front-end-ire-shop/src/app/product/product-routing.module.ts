e58b659b32641b09e9c51dbe60763c8cad549d8c
unknown <s.adeboye@outlook.com>
2024-04-06 08:26:13 +0100

    updated product home css

--- front-end-ire-shop/src/app/product/product-routing.module.ts
+++ front-end-ire-shop/src/app/product/product-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ProductHomeComponent } from "./product-home/product-home.component";
 import { ProductBagsComponent } from "./product-bags/product-bags.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 const routes: Routes = [
     {
@@ -11,6 +12,10 @@ const routes: Routes = [
     {
         path: 'bags',
         component: ProductBagsComponent
+    },
+    {
+        path: 'bags/1',
+        component: ProductDetailComponent
     }
 ];
 
